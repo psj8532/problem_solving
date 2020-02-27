5fd19ed64c97a754a544e755af666b6284da8f9f
psj8532 <psj8532@naver.com>
2020-02-27 16:57:44 +0900

    2020-02-27 문제풀이

--- baekjoon/Ad 대비/2580.py
+++ baekjoon/Ad 대비/2580.py
@@ -3,33 +3,33 @@ import sys
 sys.stdin = open("2580.text")
 
 def position_y(y):
-    if 0<=y<3:
-        a=list(range(0,3))
-    elif 3<=y<6:
-        a = list(range(3,6))
-    elif 6<=y<9:
-        a = list(range(6,9))
+    if 0 <= y < 3:
+        a = range(0,3)
+    elif 3 <= y < 6:
+        a = range(3,6)
+    elif 6 <= y < 9:
+        a = range(6,9)
     return a
 
 def position_x(x):
     if 0 <= x < 3:
-        b = list(range(0,3))
+        b = range(0,3)
     elif 3 <= x < 6:
-        b = list(range(3,6))
+        b = range(3,6)
     elif 6 <= x < 9:
-        b = list(range(6,9))
+        b = range(6,9)
     return b
 
 def check(y,x,num):
     for i in range(9):
-        if (y,x)!=(i,x) and matrix[i][x]==num:
+        if y!=i and matrix[i][x] == num:
             return False
     for j in range(9):
-        if (y,x) != (i,x) and matrix[y][j] == num:
+        if x!=j and matrix[y][j] == num:
             return False
     a = position_y(y)
     b = position_x(x)
-    for i in a: # 중복 값이 들어가버림
+    for i in a:
         for j in b:
             if (y,x)!=(i,j) and matrix[i][j] == num:
                 return False
@@ -39,40 +39,48 @@ def play(y,x,num):
     matrix[y][x]=num
     if y == 8 and x == 8:
         global ans
-        for _ in range(9):
-            print(*matrix[_])
+        for row in matrix:
+            print(*row)
         ans = 1
         return
     else:
-        for i in range(9):
-            for j in range(9):
-                if not matrix[i][j]:
-                    for next_num in range(1,10):
-                        if check(i,j,next_num):
-                            play(i,j,next_num)
-                            if ans:
-                                return
+        i=y
+        j=x+1
+        if j>=9:
+            j=0
+            i+=1
+        while i<9:
+            if not matrix[i][j]:
+                for next_num in range(1,10):
+                    if check(i,j,next_num):
+                        play(i,j,next_num)
+                        if ans:
+                            return
+                else:
                     matrix[y][x]=0
                     return
+            j += 1
+            if j == 9:
+                i += 1
+                j = 0
 
 matrix = []
 for _ in range(9):
     matrix.append(list(map(int,input().split())))
-num_list = list(range(1,10))
-IsEnd = False
 ans = 0
-for y in range(9):
-    for x in range(9):
-        if not matrix[y][x]:
-            for num in range(1, 10):
-                if check(y,x,num):
-                    play(y,x,num)
-                if ans:
-                    IsEnd = True
-                    break
-            IsEnd =True
-        if IsEnd:
-            break
-    if IsEnd:
+y=x=0
+while y<9:
+    if not matrix[y][x]:
+        for num in range(1, 10):
+            if check(y,x,num):
+                play(y,x,num)
+            if ans:
+                break
+    if ans:
         break
+    x+=1
+    if x==9:
+        y+=1
+        x=0
+
 #23:39 #시간 초과
---
+++ baekjoon/DFS/경로찾기.py
@@ -0,0 +1,45 @@
+#11403 #13:27
+import sys
+sys.stdin=open("경로찾기.text","r")
+
+#matrix에 경로 정보 받음
+#i에서 j까지의 경로(반대의 경우는 생각안해도됨
+#이중 for문으로 그 자리에 있는 i(=start)와 j(=stop) 값을 받아와서 경로 있는지 탐색
+
+def Dfs(start,end):
+    global cnt
+    cnt+=1
+    visited[start] = 1
+    if cnt==1:
+        visited[start]=0
+
+    if start == end and cnt!=1:
+        global ans
+        ans = 1
+        return
+
+    for next in range(n):
+        if matrix[start][next] and not visited[next]:
+            Dfs(next,end)
+            if ans:
+                return
+    return
+
+n=int(input())
+matrix=[]
+data = [[0]*n for _ in range(n)]
+cnt=0
+for _ in range(n):
+    matrix.append(list(map(int,input().split())))
+
+for y in range(n):
+    for x in range(n):
+        visited = [0] * n
+        ans = 0
+        cnt=0
+        Dfs(y,x)
+        data[y][x]=ans
+
+for row in data:
+    print(*row)
+#14:24
