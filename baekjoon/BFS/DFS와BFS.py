6aafe29d428b2125a87ab7b629fc90052da00410
psj8532 <psj8532@naver.com>
2020-02-29 20:12:10 +0900

    2020-02-29 BFS 문제풀이

---
+++ baekjoon/BFS/DFS와BFS.py
@@ -0,0 +1,65 @@
+#1260 #13:57
+import sys
+sys.stdin = open("DFS와BFS.text","r")
+#DFS에 관한 함수 정의(Stack 이용)
+#BFS에 관한 함수 정의(Queue 이용)
+#n:정점의 갯수, m:간선의 갯수, v:탐색을 시작할 정점의 번호
+
+def nextFind(h):
+    for next in range(1,n+1):
+        if matrix[h][next] and not visited[next]:
+            return next
+
+def bfs(here):
+    queue=[]
+    queue.append(here)
+    result.append(here)
+    visited[here]=1
+
+    while queue:
+        here = queue.pop(0)
+        for next in range(1, n+1):
+            if matrix[here][next] and not visited[next]:
+                queue.append(next)
+                result.append(next)
+                visited[next]=1
+
+    print(*result)
+
+def dfs(here): #길이 없어 돌아올때 중간지점에서 pop하면 그 지점이 스택에서 사라져서 다시 돌아올때 다른경로를 탐색못함
+    stack=[]
+    stack.append(here)
+    visited[here]=1
+    result.append(here)
+    while stack:
+        next = nextFind(here)
+        while next:
+            stack.append(next)
+            result.append(next)
+            visited[next]=1
+            next = nextFind(next)
+        here=stack.pop()
+        if nextFind(here):
+            stack.append(here)
+
+    print(*result)
+
+n,m,v=map(int,input().split())
+data=[]
+visited=[0]*(n+1)
+matrix = [[0]*(n+1) for _ in range(n+1)]
+result=[]
+for _ in range(m):
+    data.append(list(map(int,input().split())))
+
+for i in range(m):
+    start,end = data[i][0],data[i][1]
+    matrix[start][end]=1
+    matrix[end][start]=1
+
+dfs(v)
+visited=[0]*(n+1)
+result=[]
+bfs(v)
+
+#17:15
---
+++ baekjoon/BFS/미로탐색.py
@@ -0,0 +1,35 @@
+#2178 #17:33
+import sys
+sys.stdin = open("미로탐색.text","r")
+
+def bfs(y,x):
+    queue=[]
+    result=[]
+    a=1
+    queue.append([y,x,a])
+    visited[y][x]=1
+
+    while queue:
+        here = queue.pop(0)
+        a=here[2]
+        if here[0]==n-1 and here[1]==m-1:
+            return a
+        for dir in range(len(dy)):
+            y,x=here[0],here[1]
+            new_y,new_x =y+dy[dir],x+dx[dir]
+            if 0<=new_y<n and 0<=new_x<m and matrix[new_y][new_x] and not visited[new_y][new_x]:
+                queue.append([new_y,new_x,a+1])
+                visited[new_y][new_x]=1
+                result.append([new_y,new_x])
+
+
+n,m = map(int,input().split())
+dy=[-1,0,1,0]
+dx=[0,1,0,-1]
+matrix=[]
+for _ in range(n):
+    matrix.append(list(map(int,input())))
+visited=[[0]*m for _ in range(n)]
+depth = bfs(0,0)
+print(depth)
+#19:58
