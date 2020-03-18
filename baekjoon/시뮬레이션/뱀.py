98a915427533a3211557c567729173a3ea83fe7a
psj8532 <psj8532@naver.com>
2020-03-18 14:21:40 +0900

    2020-03-18 뱀 문제풀이

--- baekjoon/시뮬레이션/뱀.py
+++ baekjoon/시뮬레이션/뱀.py
@@ -1,19 +1,57 @@
 #3190 #22:25
-#처음 뱀의 이동방향은 오른쪽
-#뱀의 방향 정보는 data 리스트에 저장 #data[1]에 따라 방향 전환
-#visited에 표시하면서 이동하며, 사과가 있을시 visited 유지, 없다면 끝에거 취소
 import sys
 sys.stdin=open("뱀.text","r")
+from collections import deque
+def isExist(new_y,new_x):
+    if matrix[new_y][new_x]:
+        matrix[new_y][new_x]=0
+        return True
+    else:
+        return False
+def isSafe(new_y,new_x):
+    if 0<=new_y<n and 0<=new_x<n:
+        return True
+    else:
+        return False
+def move(dir,end,t):
+    deq=deque()
+    y=x=0
+    deq.append([y,x])
+    visited[y][x]=1
 
-n=int(input())#보드의 크기
-k=int(input())#사과의 갯수
-matrix=[[0]*n for _ in range(n)] #맵
+    dy,dx=direct[dir][0],direct[dir][1]
+    while isSafe(y+dy,x+dx) and not visited[y+dy][x+dx]:
+        t+=1
+        y+=dy
+        x+=dx
+        visited[y][x]=1
+        deq.appendleft([y, x])
+        if not isExist(y,x):
+            ty, tx = deq.pop()
+            visited[ty][tx] = 0
+        if end<len(data) and t==data[end][0]:
+            if data[end][1]=='D':
+                dir = direct.index(R[dir])
+            else:
+                dir=direct.index(L[dir])
+            end+=1
+            dy, dx = direct[dir][0], direct[dir][1]
+    return t+1
+
+n=int(input())
+k=int(input())
+matrix=[[0]*n for _ in range(n)]
+visited=[[0]*n for _ in range(n)]
+direct=[[0,1],[1,0],[0,-1],[-1,0]]
+R=[[1,0],[0,-1],[-1,0],[0,1]]
+L=[[-1,0],[0,1],[1,0],[0,-1]]
 for i in range(k):
     y,x=map(int,input().split())
     matrix[y-1][x-1]=1
-for row in matrix:
-    print(*row)
 d=int(input())
-data=[list(map(str,input().split())) for _ in range(d)] #방향 정보
+data=[list(map(str,input().split())) for _ in range(d)]
 for i in range(d):
     data[i][0] = int(data[i][0])
+
+time=move(0,0,0)
+print(time)
