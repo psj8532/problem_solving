ee3ec9cfdf4114deb2c4cad426876212e2a6ae2e
psj8532 <psj8532@naver.com>
2020-04-16 00:33:53 +0900

    2020-04-15 경사로 문제풀이

--- baekjoon/sw 역량테스트 기출/경사로.py
+++ baekjoon/sw 역량테스트 기출/경사로.py
@@ -1,112 +1,97 @@
 import sys
 sys.stdin = open("경사로.txt","r")
 
-def check_up_x(ty, tx, tl):
-    if tx - tl > 0 and Matrix[ty][tx-tl-1] <= Matrix[ty][tx-tl]:
+def check_up(y,x):
+    if x-L==0:
         return True
-    elif tx - tl == 0:
-        return True
-    else:
-        return False
-
-def check_up_y(tx, ty, tl):
-    if ty - tl > 0 and Matrix[ty-tl-1][tx] <= Matrix[ty-tl][tx]:
-        return True
-    elif ty - tl == 0:
+    elif x-L>0 and matrix[y][x-L-1]<=matrix[y][x-L]:
         return True
     else:
         return False
 
-def check_down_x(ty,tx,tv):
-    for j in range(tx+1,tx+L):
-        if tx+L<=N and tv == Matrix[ty][j]:
+def check_down(y,x,v):
+    for j in range(x+1,x+L):
+        if x+L<=N and matrix[y][j] == v:
             continue
         else:
             return False
     else:
-        if tx+L == N:
+        if x+L == N:
             return N-1
-        elif tx+L<N:
-            if Matrix[ty][tx+L-1] >= Matrix[ty][tx+L]:
-                return tx+L-1
-        else:
-            return False
-
-def check_down_y(tx,ty,tv):
-    for j in range(ty+1,ty+L):
-        if ty+L<=N and tv == Matrix[j][tx]:
-            continue
-        else:
-            return False
-    else:
-        if ty+L == N:
-            return N-1
-        elif ty+L<N:
-            if Matrix[ty+L-1][tx] >= Matrix[ty+L][tx]:
-                return ty+L-1
+        elif x+L < N and matrix[y][x+L-1] >= matrix[y][x+L]:
+            return x+L-1
         else:
             return False
 
 N,L = map(int,input().split())
-Matrix = [list(map(int,input().split())) for _ in range(N)]
+matrix = [list(map(int,input().split())) for _ in range(N)]
+matrix_rotate = [[0]*N for _ in range(N)]
 road = 0
 
-for y in range(N):
-    x = cnt = 1
-    val = Matrix[y][0]
-    while x!=N:
-        if val == Matrix[y][x]:
+for r in range(N):
+    c = cnt = 1
+    val = matrix[r][0]
+    while c!=N:
+        if matrix[r][c] == val:
             cnt += 1
-        elif val-Matrix[y][x]==-1 and x==N-1:
-            if check_up_x(y,x,L):
+        elif val-matrix[r][c]==-1 and c==N-1:
+            if cnt>=L and check_up(r,c): #마지막 열일때, cnt를 확인안해서 틀렸음
                 pass
             else:
                 break
-        elif val-Matrix[y][x]==-1 and cnt>=L:
-            if check_up_x(y,x,L):
-                cnt,val = 1, Matrix[y][x]
+        elif val-matrix[r][c]==-1 and cnt>=L:
+            if check_up(r,c):
+                cnt,val = 1,matrix[r][c]
             else:
                 break
-        elif val-Matrix[y][x]==1:
-            x = check_down_x(y,x,Matrix[y][x])
-            if x:
-                cnt,val = 0,Matrix[y][x]
+        elif val-matrix[r][c]==1:
+            c = check_down(r,c,matrix[r][c])
+            if c:
+                cnt,val = 0, matrix[r][c]
             else:
                 break
         else:
             break
-        x += 1
-    if x == N:
+        c += 1
+    if c == N:
+        # print('{} right'.format(r))
         road += 1
-        print('{} right'.format(y))
 
-for x in range(N):
-    y = cnt = 1
-    val = Matrix[0][x]
-    while y!=N:
-        if val == Matrix[y][x]:
+for j in range(N):
+    for i in range(N):
+        matrix_rotate[j][i]=matrix[i][j]
+
+for i in range(N):
+    for j in range(N):
+        matrix[i][j] = matrix_rotate[i][j]
+
+for r in range(N):
+    c = cnt =1
+    val = matrix[r][0]
+    while c!=N:
+        if matrix[r][c] == val:
             cnt += 1
-        elif val-Matrix[y][x]==-1 and y==N-1:
-            if check_up_y(x,y,L):
+        elif val-matrix[r][c]==-1 and c==N-1:
+            if cnt>=L and check_up(r,c):
                 pass
             else:
                 break
-        elif val-Matrix[y][x]==-1 and cnt>=L:
-            if check_up_y(x, y, L):
-                cnt,val = 1, Matrix[y][x]
+        elif val-matrix[r][c]==-1 and cnt>=L:
+            if check_up(r,c):
+                cnt,val = 1,matrix[r][c]
             else:
                 break
-        elif val-Matrix[y][x]==1:
-            y = check_down_y(x,y,Matrix[y][x])
-            if y:
-                cnt,val = 0,Matrix[y][x]
+        elif val-matrix[r][c]==1:
+            c = check_down(r,c,matrix[r][c])
+            if c:
+                cnt,val = 0, matrix[r][c]
             else:
                 break
         else:
             break
-        y += 1
-    if y == N:
+        c += 1
+    if c == N:
+        # print('{} down'.format(r))
         road += 1
-        print('{} down'.format(x))
 
 print(road)
