9d73b7976c8513a096473203e0cce596a13b6aab
psj8532 <psj8532@naver.com>
2020-04-19 23:18:48 +0900

    2020-04-17 알고리즘 기출문제 풀이(수정)

--- baekjoon/sw 역량테스트 기출/아기상어.py
+++ baekjoon/sw 역량테스트 기출/아기상어.py
@@ -2,6 +2,18 @@ import sys
 sys.stdin = open("아기상어.txt","r")
 from _collections import deque
 
+def select(s):
+    global charge
+    min_y, min_x, d = s[0][0], s[0][1], s[0][2]
+    for i in range(1, len(s)):
+        if s[i][0] < min_y:
+            min_y, min_x = s[i][0], s[i][1]
+        elif s[i][0] == min_y and min_x > s[i][1]:
+            min_y, min_x = s[i][0], s[i][1]
+    matrix[min_y][min_x] = 0
+    charge += 1
+    return (min_y, min_x, d)
+
 def bfs(y,x,d):
     global charge
     flag = 0
@@ -14,50 +26,27 @@ def bfs(y,x,d):
     while deq:
         here = deq.popleft()
         y,x,d = here[0],here[1],here[2]
-        if flag and s:
-            if temp==d:
-                pass
-            else:
-                min_y, min_x, d = s[0][0], s[0][1], s[0][2]
-                for i in range(1, len(s)):
-                    if s[i][0] < min_y:
-                        min_y, min_x = s[i][0], s[i][1]
-                    elif s[i][0] == min_y and min_x > s[i][1]:
-                        min_y, min_x = s[i][0], s[i][1]
-                matrix[min_y][min_x] = 0
-                charge += 1
-                return (min_y, min_x, d)
-
+        if flag and s and temp!=d:
+            return select(s)
         for next in range(len(direct)):
             new_y,new_x = y + direct[next][0],x+direct[next][1]
             if 0<=new_y<n and 0<=new_x<n and 0<matrix[new_y][new_x]<shark_size and not visited[new_y][new_x]:
                 s.append((new_y,new_x,d+1))
-                visited[new_y][new_x]=1
+                visited[new_y][new_x] = 1
             elif not s and 0<=new_y<n and 0<=new_x<n and matrix[new_y][new_x]<=shark_size and not visited[new_y][new_x]:
                 deq.append((new_y,new_x,d+1))
-                visited[new_y][new_x]=1
+                visited[new_y][new_x] = 1
         if s:
-            temp = d
-            flag = 1
+            temp,flag = d,1
             if not deq:
-                min_y, min_x, d = s[0][0], s[0][1], s[0][2]
-                for i in range(1, len(s)):
-                    if s[i][0] < min_y:
-                        min_y, min_x = s[i][0], s[i][1]
-                    elif s[i][0] == min_y and min_x > s[i][1]:
-                        min_y, min_x = s[i][0], s[i][1]
-                matrix[min_y][min_x] = 0
-                charge += 1
-                return (min_y, min_x, d)
-
+                return select(s)
     return False
 
 direct=[(-1,0),(0,1),(1,0),(0,-1)]
 n=int(input())
 matrix = [list(map(int,input().split())) for _ in range(n)]
 shark_size = 2
-charge = 0
-t=0
+charge = t = 0
 
 for i in range(n):
     for j in range(n):
@@ -72,5 +61,4 @@ while posi:
     if shark_size==charge:
         shark_size += 1
         charge = 0
-
 print(t)
