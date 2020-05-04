d284c299c8f2be5e24addae717d3c8220ecc44a9
psj8532 <psj8532@naver.com>
2020-05-04 20:21:10 +0900

    정리

--- baekjoon/sw 역량테스트 기출/파이프옮기기1.py
+++ baekjoon/sw 역량테스트 기출/파이프옮기기1.py
@@ -2,7 +2,7 @@ import sys
 sys.stdin = open("파이프옮기기1.txt","r")
 from _collections import deque
 
-def next_find_r(new_y,new_x,deq):
+def next_find_r(new_y,new_x,deq,count):
     global cnt
     if 0<=new_y<N and 0<=new_x+1<N and not matrix[new_y][new_x+1]:
         if (new_y,new_x+1)==(N-1,N-1):
@@ -15,7 +15,7 @@ def next_find_r(new_y,new_x,deq):
             return
         deq.append((new_y+1,new_x+1,2))
 
-def next_find_c(new_y,new_x,deq):
+def next_find_c(new_y,new_x,deq,count):
     global cnt
     if 0<=new_y+1<N and 0<=new_x<N and not matrix[new_y+1][new_x]:
         if (new_y+1,new_x)==(N-1,N-1):
@@ -28,7 +28,7 @@ def next_find_c(new_y,new_x,deq):
             return
         deq.append((new_y+1,new_x+1,2))
 
-def next_find_diag(new_y,new_x,deq):
+def next_find_diag(new_y,new_x,deq,count):
     global cnt
     if 0<=new_y<N and 0<=new_x+1<N and not matrix[new_y][new_x+1]:
         if (new_y,new_x+1)==(N-1,N-1):
@@ -46,22 +46,86 @@ def next_find_diag(new_y,new_x,deq):
             return
         deq.append((new_y+1,new_x+1,2))
 
-def bfs(y,x,shape):
+def bfs(y,x,shape,count):
     deq = deque()
-    deq.append((y,x,shape))
+    deq.append((y,x,shape,count))
 
     while deq:
         here = deq.popleft()
-        y,x,shape = here[0],here[1],here[2]
+        y,x,shape,count = here[0],here[1],here[2],here[3],here[4]
         if shape == 0:
-            next_find_r(y,x,deq)
+            next_find_r(y,x,deq,count)
         elif shape == 1:
-            next_find_c(y,x,deq)
+            next_find_c(y,x,deq,count)
         else:
-            next_find_diag(y,x,deq)
+            next_find_diag(y,x,deq,count)
 
 N = int(input())
 matrix = [list(map(int,input().split())) for _ in range(N)]
 cnt = 0
-bfs(0,1,0)
+bfs(0,1,0,1)
 print(cnt)
+
+# def next_find_r(new_y,new_x,deq):
+#     global cnt
+#     if 0<=new_y<N and 0<=new_x+1<N and not matrix[new_y][new_x+1]:
+#         if (new_y,new_x+1)==(N-1,N-1):
+#             cnt += 1
+#             return
+#         deq.append((new_y,new_x+1,0))
+#     if 0<=new_y+1<N and 0<=new_x+1<N and not matrix[new_y][new_x+1] and not matrix[new_y+1][new_x] and not matrix[new_y+1][new_x+1]:
+#         if (new_y+1,new_x+1)==(N-1,N-1):
+#             cnt += 1
+#             return
+#         deq.append((new_y+1,new_x+1,2))
+#
+# def next_find_c(new_y,new_x,deq):
+#     global cnt
+#     if 0<=new_y+1<N and 0<=new_x<N and not matrix[new_y+1][new_x]:
+#         if (new_y+1,new_x)==(N-1,N-1):
+#             cnt += 1
+#             return
+#         deq.append((new_y+1,new_x,1))
+#     if 0<=new_y+1<N and 0<=new_x+1<N and not matrix[new_y][new_x+1] and not matrix[new_y+1][new_x] and not matrix[new_y+1][new_x+1]:
+#         if (new_y+1,new_x+1)==(N-1,N-1):
+#             cnt += 1
+#             return
+#         deq.append((new_y+1,new_x+1,2))
+#
+# def next_find_diag(new_y,new_x,deq):
+#     global cnt
+#     if 0<=new_y<N and 0<=new_x+1<N and not matrix[new_y][new_x+1]:
+#         if (new_y,new_x+1)==(N-1,N-1):
+#             cnt += 1
+#             return
+#         deq.append((new_y,new_x+1,0))
+#     if 0<=new_y+1<N and 0<=new_x<N and not matrix[new_y+1][new_x]:
+#         if (new_y+1,new_x)==(N-1,N-1):
+#             cnt += 1
+#             return
+#         deq.append((new_y+1,new_x,1))
+#     if 0<=new_y+1<N and 0<=new_x+1<N and not matrix[new_y][new_x+1] and not matrix[new_y+1][new_x] and not matrix[new_y+1][new_x+1]:
+#         if (new_y+1,new_x+1)==(N-1,N-1):
+#             cnt += 1
+#             return
+#         deq.append((new_y+1,new_x+1,2))
+#
+# def bfs(y,x,shape):
+#     deq = deque()
+#     deq.append((y,x,shape))
+#
+#     while deq:
+#         here = deq.popleft()
+#         y,x,shape = here[0],here[1],here[2]
+#         if shape == 0:
+#             next_find_r(y,x,deq)
+#         elif shape == 1:
+#             next_find_c(y,x,deq)
+#         else:
+#             next_find_diag(y,x,deq)
+#
+# N = int(input())
+# matrix = [list(map(int,input().split())) for _ in range(N)]
+# cnt = 0
+# bfs(0,1,0)
+# print(cnt)
