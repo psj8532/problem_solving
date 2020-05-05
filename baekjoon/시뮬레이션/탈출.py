353cb90804d4b0e00839bfcd1ca0d829ac2353b6
psj8532 <psj8532@naver.com>
2020-05-05 18:03:00 +0900

    0504 문제풀이

--- baekjoon/시뮬레이션/탈출.py
+++ baekjoon/시뮬레이션/탈출.py
@@ -2,7 +2,7 @@ import sys
 sys.stdin = open("탈출.txt", "r")
 from _collections import deque
 
-def bfs(sy,sx,gy,gx,d,water):
+def bfs(sy,sx,d,water):
     global cnt_w
     schrenk = deque()
     schrenk.append((sy,sx,d))
@@ -11,14 +11,14 @@ def bfs(sy,sx,gy,gx,d,water):
 
     while schrenk: #고슴도치가 나중에 이동하므로 물이 다 찼아도 마지막에 고슴도치가 이동할 기회는 줘야함
         while cnt_w:
-            here_w = water.popleft()
-            wy,wx = here_w[0],here_w[1]
+            here = water.popleft()
+            y,x = here[0],here[1]
             cnt_w -= 1
             for dir in range(4):
-                new_wy, new_wx = wy + direct[dir][0], wx + direct[dir][1]
-                if 0 <= new_wy < r and 0 <= new_wx < c and matrix[new_wy][new_wx] != 'X' and matrix[new_wy][new_wx] != 'D' and not visited_w[new_wy][new_wx]:
-                    water.append((new_wy, new_wx))
-                    visited_w[new_wy][new_wx] = 1
+                new_y, new_x = y + direct[dir][0], x + direct[dir][1]
+                if 0 <= new_y < r and 0 <= new_x < c and matrix[new_y][new_x] != 'X' and matrix[new_y][new_x] != 'D' and not visited_w[new_y][new_x]:
+                    water.append((new_y, new_x))
+                    visited_w[new_y][new_x] = 1
                     count_w += 1
         while cnt_s:
             here = schrenk.popleft()
@@ -45,9 +45,7 @@ water = deque()
 cnt_w = 0
 for i in range(r):
     for j in range(c):
-        if matrix[i][j]=='D':
-            gy,gx = i,j
-        elif matrix[i][j]=='S':
+        if matrix[i][j]=='S':
             sy,sx = i,j
             visited_s[i][j] = 1
         elif matrix[i][j] == '*':
@@ -55,7 +53,7 @@ for i in range(r):
             visited_w[i][j] = 1
             cnt_w += 1
 
-result = bfs(sy,sx,gy,gx,0,water)
+result = bfs(sy,sx,0,water)
 if result:
     print(result)
 else:
