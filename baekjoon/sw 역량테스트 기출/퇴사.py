83889a4918935f1e935114876bad23adc52f6268
psj8532 <psj8532@naver.com>
2020-05-08 21:10:55 +0900

    0508 문제풀이

--- baekjoon/sw 역량테스트 기출/퇴사.py
+++ baekjoon/sw 역량테스트 기출/퇴사.py
@@ -1,48 +1,61 @@
-# 퇴사 # 18:10
-
 import sys
 sys.stdin=open("퇴사.text","r")
-def Schedule(now,n):
-    global val, result
-    if now < n and now+data[now][0]<=n+1:#다음게 n을 넘어가면 안됨
-        val += data[now][1]
-    elif now < n and now+data[now][0]>n+1:
-        if val > result:
-            result = val
-        return
-    elif now==n:
-        if data[n][0]<=1:
-            val += data[now][1]
-        else:
-            if val > result:
-                result = val
-            return
-    elif now>=n+1:
-        if val > result:
-            result = val
-        return
 
-    next = now + data[now][0]
-    if next==n+1:
-        Schedule(next,n)
-    else:
-        for idx in range(next,n+1):
-            Schedule(idx,n)
-            if idx<n:
-                if idx + data[idx][0] <= n + 1:  # 다음게 n을 넘어가면 안됨
-                    val -= data[idx][1]
-                elif idx + data[idx][0] > n + 1:
-                    return
-            elif idx == n and data[n][0]<=1:
-                val -= data[idx][1]
-    return
+def solve(start, sum):
+    global max
+    for i in range(start,N,1):
+        if i+matrix[i][0]<=N:
+            solve(i+matrix[i][0], sum+matrix[i][1])
+    if sum>max:
+        max = sum
 
-n = int(input())
-data = [[0]*2]
-for _ in range(n):
-    data.append(list(map(int,input().split())))
-result=0
-val=0
-for i in range(1,n+1):
-    Schedule(i,n)
-print(result)
+N = int(input())
+matrix = [list(map(int,input().split())) for _ in range(N)]
+max = 0
+solve(0, 0)
+print(max)
+
+# def Schedule(now,n):
+#     global val, result
+#     if now < n and now+data[now][0]<=n+1:#다음게 n을 넘어가면 안됨
+#         val += data[now][1]
+#     elif now < n and now+data[now][0]>n+1:
+#         if val > result:
+#             result = val
+#         return
+#     elif now==n:
+#         if data[n][0]<=1:
+#             val += data[now][1]
+#         else:
+#             if val > result:
+#                 result = val
+#             return
+#     elif now>=n+1:
+#         if val > result:
+#             result = val
+#         return
+#
+#     next = now + data[now][0]
+#     if next==n+1:
+#         Schedule(next,n)
+#     else:
+#         for idx in range(next,n+1):
+#             Schedule(idx,n)
+#             if idx<n:
+#                 if idx + data[idx][0] <= n + 1:  # 다음게 n을 넘어가면 안됨
+#                     val -= data[idx][1]
+#                 elif idx + data[idx][0] > n + 1:
+#                     return
+#             elif idx == n and data[n][0]<=1:
+#                 val -= data[idx][1]
+#     return
+#
+# n = int(input())
+# data = [[0]*2]
+# for _ in range(n):
+#     data.append(list(map(int,input().split())))
+# result=0
+# val=0
+# for i in range(1,n+1):
+#     Schedule(i,n)
+# print(result)
