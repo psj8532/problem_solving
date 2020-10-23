6d4d8321b3f498b12b80c0beed1b5dec5fa6bb26
psj8532 <psj8532@naver.com>
2020-10-23 20:04:02 +0900

    [UPDATE] solved an unsolved problem in kakao

--- Programmers/2019_BLIND_KAKAO/실패율.py
+++ Programmers/2019_BLIND_KAKAO/실패율.py
@@ -1,24 +1,24 @@
 def solution(N, stages):
     answer = []
-    fail = [0] * (N+1)
-    total = [0] * (N+1)
+    matrix = [[0]*2 for _ in range(N+2)]
     rate = [[0,i] for i in range(N+1)]
-    for i in range(len(stages)):
-        if stages[i] <= N:
-            fail[stages[i]] += 1
-        for j in range(1,stages[i]+1):
-            if j > N: break
-            total[j] += 1
-    for i in range(1, N+1):
-        rate[i][0] = fail[i] / total[i]
-    rate.pop(0)
+    for stage in stages:
+        matrix[stage][0] += 1
+    matrix[N+1][1] = matrix[N+1][0]
+    for i in range(N,0,-1):
+        matrix[i][1] = matrix[i+1][1] + matrix[i][0]
+        if matrix[i][1] == 0:
+            rate[i][0] = 0
+        else:
+            rate[i][0] = matrix[i][0] / matrix[i][1]
 
     rate.sort(reverse=True, key=lambda x:x[0])
-    print(rate)
     for i in range(len(rate)):
+        if rate[i][1] == 0: continue
         answer.append(rate[i][1])
     return answer
 
 
 ex1 = [5, [2, 1, 2, 6, 2, 4, 3, 3]]
-print(solution(*ex1))
+ex2 = [4, [4,4,4,4]]
+print(solution(*ex2))
