96adfba1e59542de0aeca97124b87f32814d04f2
psj8532 <psj8532@naver.com>
2020-10-30 00:22:35 +0900

    [ADD] solvinge the problem in kakao

--- Programmers/2019_BLIND_KAKAO/무지의먹방라이브_효율.py
+++ Programmers/2019_BLIND_KAKAO/무지의먹방라이브_효율.py
@@ -1,16 +1,20 @@
 def solution(food_times, k):
     answer = 0
+    if sum(food_times) <= k: return -1
     foods = []
-    for i in range(len(food_times)):
-        foods.append([food_times[i], i])
+    for idx,val in enumerate(food_times):
+        foods.append([val,idx+1])
     foods.sort(key=lambda x:x[0])
-    total = 0
-    mv = 0
+    total = cnt = min_val = 0
     for i in range(len(foods)):
-        mv = foods[i][0]
-        if mv * (len(foods)-i) >= k-total:
-            answer = (len(foods)-i) % (k-total) + 1
-
+        cur_val = foods[i][0]
+        if min_val == cur_val: continue
+        if (cur_val-cnt) * (len(foods)-i) > k - total:
+            answer = foods[i + ((k-total) % (len(foods)-i))][1]
+        else:
+            total += (cur_val-cnt) * (len(foods)-i)
+            cnt += (cur_val-cnt)
+            min_val = cur_val
 
     return answer
 
