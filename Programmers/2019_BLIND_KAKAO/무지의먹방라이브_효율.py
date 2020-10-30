32d26eabf04fd08db157f5ae087aafa429097097
psj8532 <psj8532@naver.com>
2020-10-30 22:57:40 +0900

    [ADD] solved the problem in kakao

--- Programmers/2019_BLIND_KAKAO/무지의먹방라이브_효율.py
+++ Programmers/2019_BLIND_KAKAO/무지의먹방라이브_효율.py
@@ -6,17 +6,27 @@ def solution(food_times, k):
         foods.append([val,idx+1])
     foods.sort(key=lambda x:x[0])
     total = cnt = min_val = 0
-    for i in range(len(foods)):
-        cur_val = foods[i][0]
-        if min_val == cur_val: continue
-        if (cur_val-cnt) * (len(foods)-i) > k - total:
-            answer = foods[i + ((k-total) % (len(foods)-i))][1]
+    for i, data in enumerate(foods):
+        val, idx = data
+        cur, remain = val - cnt, len(foods)-i
+        if min_val == val: continue
+        if cur * remain > k - total:
+            remain_lst = foods[i:]
+            remain_lst.sort(key = lambda x:x[1])
+            return remain_lst[(k-total) % remain][1]
         else:
-            total += (cur_val-cnt) * (len(foods)-i)
-            cnt += (cur_val-cnt)
-            min_val = cur_val
+            total += cur * remain
+            cnt += cur
+            min_val = val
 
     return answer
 
 ex1 = [[3,1,2], 5]
-print(solution(*ex1))
+ex2 = [[10,10,50,50,55], 168]
+ex3 = [[5,10,5], 14]
+ex4 = [[7,8,3,3,2,2,2,2,2,2,2,2], 35]
+ex5 = [[3,1,1,1,2,4,3], 12]
+ex6 = [[4,3,5,6,2], 7]
+ex7 = [[4,1,1,5], 4]
+ex8 = [[4,2,3],5]
+print(solution(*ex6))
