7f10e343f2201fe8017349dad8714c3e24445dc6
psj8532 <psj8532@naver.com>
2020-11-06 22:58:36 +0900

    [ADD] solved the prooblem using sorting algorithm

--- BOJ/정렬/수정렬하기2.py
+++ BOJ/정렬/수정렬하기2.py
@@ -3,29 +3,32 @@
 # 병합정렬의 경우 최악의 경우도 nlogn이므로 병합정렬을 이용하면 풀릴 것으로 예상
 # 파이썬의 소트는 통과
 
-def partition(arr, start, end):
-    p = arr[end]
-    idx = start - 1
+# def partition(arr, start, end):
+#     p = arr[end]
+#     idx = start - 1
+#
+#     for j in range(start,end):
+#         if arr[j] < p:
+#             idx += 1
+#             arr[idx], arr[j] = arr[j], arr[idx]
+#     arr[idx+1], arr[end] = arr[end], arr[idx+1]
+#     return idx + 1
+#
+#
+# def quick_sort(arr, l, r):
+#     if l < r:
+#         pivot = partition(arr, l, r)
+#         quick_sort(arr, 0, pivot-1)
+#         quick_sort(arr, pivot+1, r)
 
-    for j in range(start,end):
-        if arr[j] < p:
-            idx += 1
-            arr[idx], arr[j] = arr[j], arr[idx]
-    arr[idx+1], arr[end] = arr[end], arr[idx+1]
-    return idx + 1
-
-
-def quick_sort(arr, l, r):
-    if l < r:
-        pivot = partition(arr, l, r)
-        quick_sort(arr, 0, pivot-1)
-        quick_sort(arr, pivot+1, r)
+def merge_sort(arr, l, r):
+    pass
 
 
 N = int(input())
 answer = []
 for _ in range(N):
     answer.append(int(input()))
-quick_sort(answer, 0, len(answer)-1)
+merge_sort(answer, 0, len(answer)-1)
 for num in answer:
     print(num)
