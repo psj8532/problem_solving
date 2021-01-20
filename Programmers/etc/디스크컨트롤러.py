5f0955dc14d6c6626b5a10a85dde31802ab53f75
psj8532 <psj8532@naver.com>
2021-01-20 16:18:53 +0900

    Add problem_solving

--- Programmers/etc/디스크컨트롤러.py
+++ Programmers/etc/디스크컨트롤러.py
@@ -1,56 +1,24 @@
 # 10:25 ~
+from _collections import deque
 import heapq
 
 def solution(jobs):
-    def get_min(k,tt,ett):
-        nonlocal min_v, min_idx
-        if k == len(cand):
-            if ett < min_v:
-                min_v = ett
-                min_idx = visit[0]
-
-            return
-        for i in range(len(cand)):
-            if not visited[i]:
-                visited[i] = 1
-                visit.append(i)
-                s,e = cand[i]
-                total = (tt-s) + e
-                get_min(k+1,tt+e,ett + total)
-                visit.pop()
-                visited[i] = 0
-        return
     answer = 0
-    cnt = len(jobs)
-    heapq.heapify(jobs)
-    t = 0
-    while jobs:
-        cand = []
-        while jobs:
-            s,e = heapq.heappop(jobs)
-            if s <= t:
-                cand.append([s,e])
-            elif not cand and t <= s:
-                cand.append([s,e])
-            elif not cand and t < s:
-                break
-            else:
-                heapq.heappush(jobs, [s,e])
-                break
-
-        min_v = 9876543210
-        min_idx = -1
-        visit = []
-        visited = [0] * len(cand)
-        get_min(0,t,answer)
-        for i in range(len(cand)):
-            if i == min_idx: continue
-            heapq.heappush(jobs, cand[i])
-        answer += (t-cand[min_idx][0]) + cand[min_idx][1]
-        t += cand[min_idx][1]
-        # print('총합시간: ', answer, '완료시간: ', t)
-    return answer // cnt
-
+    waiting = deque(sorted(jobs))
+    curr_time, done, cand = 0, 0, []
+    while done < len(jobs):
+        if cand:
+            time, start = heapq.heappop(cand)
+            curr_time += time
+            answer += curr_time - start
+        else:
+            start, time = waiting.popleft()
+            curr_time = start + time
+            answer += time
+        done += 1
+        while waiting and waiting[0][0] <= curr_time:
+            heapq.heappush(cand, waiting.popleft()[::-1])
+    return answer // len(jobs)
 
 ex1 = [[0, 3], [1, 9], [2, 6]]  # 9
 print(solution(ex1))
