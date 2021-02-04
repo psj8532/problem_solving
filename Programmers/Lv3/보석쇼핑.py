39f50fb24d44d548f93d5dc1b16f1cc2d107edfb
psj8532 <psj8532@naver.com>
2021-02-04 19:31:19 +0900

    Add problem in Programmers

--- Programmers/Lv3/보석쇼핑.py
+++ Programmers/Lv3/보석쇼핑.py
@@ -1,4 +1,4 @@
-# # 효율성 6개 맞음
+# # 효율성 6개 맞음 => 7 => 다 맞음
 def solution(gems):
     def check(s, e, l):
         if not answer: return True
@@ -7,19 +7,22 @@ def solution(gems):
         return False
 
     answer, G, min_v = [], len(set(gems)), 0
-    cand = set()
-    gem_idx = {}
+    gem_idx, cand = {}, set()
+    start = 1
     for i, gem in enumerate(gems):
-        gem_idx[gem] = i + 1
+        cur = i + 1
+        if gem in gem_idx and gem_idx[gem] == start:
+            ts = cur
+            for g in gem_idx:
+                if gem != g and gem_idx[g] < ts:
+                    ts = gem_idx[g]
+            start = ts
+        gem_idx[gem] = cur
         cand.add(gem)
         if len(cand) != G: continue
-        idxs = []
-        for g in gem_idx:
-            idxs.append(gem_idx[g])
-        start, end = min(idxs), max(idxs)
-        if check(start, end, end - start + 1):
-            min_v = end - start + 1
-            answer = [start, end]
+        if check(start, cur, cur - start + 1):
+            min_v = cur - start + 1
+            answer = [start, cur]
     return answer
 
 ex1 = ["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"] # [3, 7]
@@ -27,6 +30,3 @@ ex2 = ["AA", "AB", "AC", "AA", "AC"]    # [1, 3]
 ex3 = ["XYZ", "XYZ", "XYZ"] # [1, 1]
 ex4 = ["ZZZ", "YYY", "NNNN", "YYY", "BBB"] # [1,5]
 print(solution(ex1))
-print(solution(ex2))
-print(solution(ex3))
-print(solution(ex4))
