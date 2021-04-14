771d87221a0c201be458d63abe8c378481b350ca
psj8532 <psj8532@naver.com>
2021-04-14 21:56:32 +0900

    Add problem in Programmers

--- Programmers/Lv2/문자열압축.py
+++ Programmers/Lv2/문자열압축.py
@@ -1,39 +1,25 @@
-# 18:53 ~ 19:52
 def solution(s):
-    answer = 1001
-    for cnt in range(1,len(s)+1):
-        c_lst = []
-        s_lst = []
-        for start in range(0,len(s),cnt):
-            end = start + cnt
-            word = s[start:end]
-            c_lst.append(word)
-
-        for i in range(len(c_lst)):
-            if i == 0:
-                cnt = 1
-            elif c_lst[i-1] == c_lst[i]:
-                cnt += 1
+    answer = S = len(s)
+    half = S // 2
+    for cutting in range(1, half + 1):
+        words = {}
+        word_key = []
+        ns = ''
+        for i in range(0, S, cutting):
+            word = s[i:i+cutting]
+            if word_key and word_key[-1] == word:
+                words[word] += 1
             else:
-                if cnt != 1:
-                    s_lst.append(str(cnt))
-                s_lst.append(c_lst[i-1])
-                cnt = 1
-        else:
-            if cnt != 1:
-                s_lst.append(str(cnt))
-            s_lst.append(c_lst[-1])
-
-        s_str = "".join(s_lst)
-        answer = min(answer,len(s_str))
-
+                if word_key and word_key[-1] != word:
+                    prev_word = word_key.pop(0)
+                    if words[prev_word] == 1: ns += prev_word
+                    else: ns += str(words[prev_word]) + prev_word
+                    del words[prev_word]
+                word_key.append(word)
+                words[word] = 1
+        if word_key:
+            last_word = word_key.pop(0)
+            if words[last_word] == 1: ns += last_word
+            else: ns += str(words[last_word]) + last_word
+        answer = min(answer, len(ns))
     return answer
-
-# s	result
-ex1 = "aabbaccc"	# 7
-ex2 = "ababcdcdababcdcd"	# 9
-ex3 = "abcabcdede"	# 8
-ex4 = "abcabcabcabcdededededede"	# 14
-ex5 = "xababcdcdababcdcd"	# 17
-ex6 = 'a' # 1
-print(solution(ex6))
